33bc4948431610c770ec0097386603be4aaca5f6
kb5fls <75853395+kb5fls@users.noreply.github.com>
2022-06-22 15:44:18 -0500

    Test Commit

---
+++ Training/addnumbeers.py
@@ -0,0 +1,8 @@
+
+
+x = input("Please enter in a number: ")
+y = input("Please enter in your second number: ")
+
+z = int(x) + int(y)
+
+print("The number ", str(x), " and the number ", str(y),  " added together is equal to: " + str(z))
