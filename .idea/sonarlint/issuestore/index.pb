0f1029f440d9554a78c642d9c294899b8a3dc517
kb5fls <salem.philip@gmail.com>
2022-08-15 06:35:52 -0500

    Created Servicerules-test.py file to add code to remove special characters in file output file.

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,17 @@
+
+9
+	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+=
+response.json,e\6\e6d536c834eea55d3b23232379b301b97fef2cb4
+9
+	rules.txt,1\6\160dbb7947bec2b35d722d7d275b5d9270225daa
+\
+,.idea/inspectionProfiles/Project_Default.xml,4\9\496a238a6afa168dbaf6efd37bb459331589579c
+S
+#.idea/sonarlint/issuestore/index.pb,9\f\9fe84ebb15faf917b7def6236dba604453cc61e0
+9
+	.dcignore,7\7\7791000a9a93019a87d67e0b676c86447ba29c67
+^
+..idea/inspectionProfiles/profiles_settings.xml,1\e\1e9075f5bf079c01ef2c910709e91a497d262080
+?
+ServiceRules.py,b\b\bb25e51c91d6fe9a6f0647a2329b7824d53cd107
---
+++ ServiceRules-Test.py
@@ -0,0 +1,58 @@
+import requests
+import json
+import os
+import tarfile
+import sys
+
+# Author: Philip Salem
+'''
+This script will extract all Cloud Conformity rules supported for AWS, Azure, and GCP
+then will create a rules.txt file logging every rule including the Rule ID, Rule Title,
+Rule Category, and the Cloud Provider the rule belongs to. 
+'''
+
+# API key is configured as env variable for Windows OS.
+apikey = os.environ.get('API_KEY')
+# print(apikey)
+url = "https://us-west-2-api.cloudconformity.com/v1/services"
+
+payload = {}
+headers = {
+    'Content-Type': 'application/vnd.api+json',
+    'Authorization': f'ApiKey {apikey}'
+}
+
+
+response = requests.get(url, headers=headers, data=payload)
+
+acct_data = response.json()
+
+# Creates service_rules.json file to hold rules list in JSON format.
+with open('service_rules.json', 'w') as json_file:
+    json.dump(acct_data, json_file, indent=4)
+
+# Parse rules from service_rules.json file and then writes those rules to rules.txt
+with open('service_rules.json', 'r') as json_file:
+    json_load = json.load(json_file)
+# print(json_load['web']['languages']['id'])
+original_stdout = sys.stdout
+with open('rules.txt', 'w') as r:
+    sys.stdout = r
+    data = json_load['included']
+
+
+    for x in data:
+        if x['provider'] == 'gcp' or x['provider'] == 'aws' or x['provider'] == 'azure':
+            print("Rule ID: ", x['id'], "\n", "Rule Title: ", x['title'], "\n", "Rule Category: ", x['categories'], "\n",
+              "Cloud Provider: ", x['provider'], "\n\n")
+
+'''
+JSON data containts square [, ], and '
+The code below removes characters from rules.txt file and writes the final output to rules-list.txt 
+and then deletes the temporary rules.txt file
+'''
+with open('rules.txt', 'r') as infile, open('rules-list.txt', 'w') as outfile:
+        temp = infile.read().replace("[", "").replace("]", "").replace("'", "")
+        outfile.write(temp)
+
+os.remove("rules.txt")
